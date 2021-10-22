887356e9199c46aa7c9e37dfce5f97a8c8b0237b
Daniel Kinahan <danielkinahan@uvic.ca>
2021-10-21 22:11:05 -0700

    Completed skeleton technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,48 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+    <Section id="tech">
+        <br />
+        <SectionDivider />
+        <SectionTitle>Technologies</SectionTitle>
+        <SectionText>
+            Worked with a range of technologies to blah blah blah.
+        </SectionText>
+        <List>
+            <ListItem>
+                <DiFirebase size="3rem" />
+                <ListContainer>
+                    <ListTitle>Front-End</ListTitle>
+                    <ListParagraph>
+                    Experienced  with <br />
+                    React.js
+                    </ListParagraph>
+                </ListContainer>
+            </ListItem>
+            <ListItem>
+                <DiReact size="3rem" />
+                <ListContainer>
+                    <ListTitle>Back-End</ListTitle>
+                    <ListParagraph>
+                        Experienced  with <br />
+                        Node and databases
+                    </ListParagraph>
+                </ListContainer>
+            </ListItem>
+            <ListItem>
+                <DiZend size="3rem" />
+                <ListContainer>
+                    <ListTitle>Front-End</ListTitle>
+                    <ListParagraph>
+                        Experienced  with <br />
+                        React.js
+                    </ListParagraph>
+                </ListContainer>
+            </ListItem>
+        </List>
+    </Section>
+
 );
 
 export default Technologies;
+
